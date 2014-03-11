f05da531fca1626c39323dcc6cd72d354f54a3d5
santosh <pinki@leotechpinki.(none)>
2014-03-11 12:34:27 +0530

    First Release

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+	before_filter :authenticate_user!, :only => :index
+ 
+  def index
+    @role = session[:role]
+  	@events_attended = current_user.registered_events     
+  	@events_published = Event.where(published: true)
+      if @role =="host"
+  	    @events_for_publish = current_user.events.where(published: false)
+        @events_published_by_you = current_user.events.where(published: true)
+      end
+  end
+  
+  def about
+  end
+
+end
