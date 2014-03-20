a81c346ad577744da3a07d68bf477cb8cb9f58bb
santosh <pinki@leotechpinki.(none)>
2014-03-20 16:49:56 +0530

    Changed Production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ EventCircle::Application.configure do
   config.action_mailer.default_url_options = { :host => '54.85.83.14' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -48,7 +48,7 @@ EventCircle::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( *.js *.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
