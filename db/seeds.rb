d86de6f26573e806fcb5441b37675338083bec94
Santosh <santoshk@leosantosh.(none)>
2014-06-25 12:31:03 +0530

    changed Indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
-Event.delete_all
 
 @host1 =User.create(uname: "host1", email: "host@test.com", password: "host1234", password_confirmation: "host1234" )
  @host1.add_role :host
@@ -19,28 +18,5 @@ Event.delete_all
  @host.add_role :admin
  @host.save
 
- @event1=Event.create(title: "Event1", description: "some", venue: "pune", published:true, 
- 	published_at: Time.now,event_start_date:Time.new(2014, 3, 14, 12, 2, 2, "+02:00"),
- 	event_end_date: Time.new(2014,03,17,12,27,24) )
- @host1.events << @event1
-
-  @event2=Event.create(title: "Event1", description: "some", venue: "pune", published:true, 
- 	published_at: Time.now,event_start_date:Time.new(2014, 3, 14, 12, 2, 2, "+02:00"),
- 	event_end_date: Time.new(2014,03,17,12,27,24) )
- @host1.events << @event2
-
-  @event3=Event.create(title: "Event1", description: "some", venue: "pune", published:true, 
- 	published_at: Time.now,event_start_date:Time.new(2014, 3, 14, 12, 2, 2, "+02:00"),
- 	event_end_date: Time.new(2014,03,17,12,27,24) )
- @host1.events << @event3
-
- @event4=Event.create(title: "Event2", description: "some desc2", venue: "pune")
- @host1.events << @event4
-
- @event5=Event.create(title: "Event2", description: "some desc2", venue: "pune")
- @host1.events << @event5
-
- @event6=Event.create(title: "Event2", description: "some desc2", venue: "pune")
- @host1.events << @event6
 
  
